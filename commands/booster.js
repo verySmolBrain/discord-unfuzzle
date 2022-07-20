1f564f0835be26fb53ba8df7759a51bf9055584f
Brendon Zhao <brendon-z@Axiom-V3.localdomain>
2022-07-20 15:53:50 +1000

    implemented user commands and implemented a check for whether a user is boosted (command)

---
+++ commands/booster.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const moment = require('moment');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('booster')
+		.setDescription('Checks whether the target is a booster or not!')
+		.addUserOption(option => option.setName('target').setDescription('Select a user')),
+	async execute(interaction) {
+    let target = interaction.options.getUser('target');
+
+    if (!target) {
+      target = interaction.user
+    }
+    let member = interaction.guild.members.cache.get(target.id);
+
+    if (member.premiumSince) {
+      await interaction.reply(`${target.username} is a server booster and is very cool`);
+    } else {
+      await interaction.reply(`${target.username} is not a server booster and is very uncool`);
+    }
+
+	},
+};
--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,54 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const moment = require('moment');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Replies with user info!'),
+		.setDescription('Replies with user info!')
+		.addUserOption(option => option.setName('target').setDescription('Select a user')),
 	async execute(interaction) {
-		await interaction.reply('Not implemented yet!');
+    let target = interaction.options.getUser('target');
+    let member = interaction.guild.members.cache.get(target.id);
+  
+    const userInfo = {
+      color: 0x8300FF,
+      title: target.username + '#' + target.discriminator,
+      url: 'https://bit.do/YeetYeet',
+      author: {
+        name: 'unfuzzle',
+        icon_url: 'https://i.imgur.com/AfFp7pu.png',
+        url: 'https://bit.do/YeetYeet',
+      },
+      description: `${target.username}\'s stats`,
+      thumbnail: {
+        url: target.displayAvatarURL({ dynamic: true }),
+      },
+      fields: [
+        {
+          name: 'Roles',
+          value: `${member.roles.cache.map(r => r).join(' ')}`,
+        },
+        {
+          name: 'Member since',
+          value: `${moment(member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}`,
+          inline: true,
+        },
+        {
+          name: 'Discord member since',
+          value: `${moment(target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`,
+          inline: true,
+        },
+      ],
+      image: {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Double-compound-pendulum.gif',
+      },
+      timestamp: new Date(),
+      footer: {
+        text: 'Lorem Ipsum is my favourite ice cream',
+        icon_url: 'https://i.imgur.com/AfFp7pu.png',
+      },
+    };
+
+		await interaction.reply({ embeds: [userInfo]});
 	},
 };
